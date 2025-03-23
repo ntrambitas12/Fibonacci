8c632b9be9c1349d0c487322b2ae4efc16e257c7
Tate Moorhouse <Tate.Moorhouse@ge.com>
2025-03-23 00:32:52 -0400

    Added a basic checkstyle configuration and the checkstyle plugin to POM.xml

--- src/test/TestFibonacci.java
+++ src/test/TestFibonacci.java
@@ -7,19 +7,16 @@ public class TestFibonacci
 	@Test
 	public void test()
 	{
-		// commit test
+
 		assertEquals (0, Fibonacci.calculate(0));
-
-
 		assertEquals(1, Fibonacci.calculate(1));
 		assertEquals(1, Fibonacci.calculate(2));
 		assertEquals(2, Fibonacci.calculate(3));
-		assertEquals(3,    Fibonacci.calculate(4));
-
+		assertEquals(3, Fibonacci.calculate(4));
 		assertEquals(5, Fibonacci.calculate(5));
 		assertEquals(8, Fibonacci.calculate(6));
 		assertEquals(13, Fibonacci.calculate(7));
-		
+	
 		assertEquals(1134903170, Fibonacci.calculate(45));
 	}
 }

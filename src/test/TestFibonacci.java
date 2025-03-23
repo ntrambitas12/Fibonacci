5a8e9b7ce3207c3fe94ad4687701f33416a16f10
Tate Moorhouse <Tate.Moorhouse@ge.com>
2025-03-23 01:29:28 -0400

    Added a basic checkstyle configuration and the checkstyle plugin to POM.xml

--- src/test/TestFibonacci.java
+++ src/test/TestFibonacci.java
@@ -8,7 +8,7 @@ public class TestFibonacci
 	public void test()
 	{
 
-		assertEquals (0, Fibonacci.calculate(0));
+		  assertEquals (0, Fibonacci.calculate(0));
 		assertEquals(1, Fibonacci.calculate(1));
 		assertEquals(1, Fibonacci.calculate(2));
 		assertEquals(2, Fibonacci.calculate(3));
